1c1a83ce5c694738ec9b9d742d7eeb685b17ecbd
LornahMunene <munenelornah9@gmail.com>
2024-11-18 13:21:31 +0300

    adds routing

---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About Me</h1>
+      <p>I am a [Your Profession/Role].</p>
+      <p>Write a brief introduction about yourself here.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <div className="projects">
+      <h1>My Projects</h1>
+      <ul>
+        <li>Project 1</li>
+        <li>Project 2</li>
+        <li>Project 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
